85ec4bca2399c897153846815fc825dabde7f484
jameshtlee <jameshtlee@live.com.sg>
2020-06-13 21:31:47 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Production webapp for hack
---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', (req, res) => {
+    res.render('index');
+});
+app.use('/jane-doe', (req, res) => {
+    res.render('jane-doe');
+});
+
+module.exports = app;
